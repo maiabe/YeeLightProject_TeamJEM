6c1d4b0895f26a9d8b02e66c73fe7ff7e8b2d878
Julie Chai <juliechai75@gmail.com>
2019-04-16 18:15:50 -1000

    Only create one profile

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -17,7 +17,6 @@ class NavBar extends React.Component {
         </Menu.Item>
         {this.props.currentUser ? (
             [ <Menu.Item as={NavLink} activeClassName="active" exact to="/userhome">Home</Menu.Item>,
-              <Menu.Item as={NavLink} activeClassName="active" exact to="/input" key='input'>Input Data</Menu.Item>,
               <Menu.Item as={NavLink} activeClassName="active" exact to="/profile" key='profile'>Profile</Menu.Item>]
         ) : ''}
         {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
--- app/imports/ui/pages/Profile.jsx
+++ app/imports/ui/pages/Profile.jsx
@@ -1,42 +1,50 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Container, Header, Loader } from 'semantic-ui-react';
+import { Container, Header, Segment, Button } from 'semantic-ui-react';
 import { Profiles } from '/imports/api/profile/profile';
-import ProfileComp from '/imports/ui/components/ProfileComp';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-/** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
+/** Renders a table containing all of the Profile documents. */
 class Profile extends React.Component {
 
-  /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
-  render() {
-    return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
-  }
-
   /** Render the page once subscriptions have been received. */
-  renderPage() {
-    return (
-        <Container>
-          <Header as="h2" textAlign="center">Your Profile</Header>
-          {this.props.profile.map((profile) => <ProfileComp key={profile._id} profile={profile} />)}
-        </Container>
-    );
+  render() {
+    if (this.props.profile != null) {
+      return (
+          <Container>
+            <Header as="h2" textAlign="center">Your Profile</Header>
+            <Segment>
+              Birthday: {this.props.profile.birthday.toString()}<br/><br/>
+              Last period: {this.props.profile.last_period.toString()}<br/><br/>
+              Average cycle duration: {this.props.profile.cycle} days<br/><br/>
+              Average period duration: {this.props.profile.duration} days<br/>
+            </Segment>
+            <Link to={`/edit/${this.props.profile._id}`}>Edit</Link>
+          </Container>
+      );
+    } else {
+      return (
+          <Container>
+            <Link to={`/input`}>
+            <Button id='profileButton' color='red'>Create Profile</Button>
+            </Link>
+          </Container>
+      );
+    }
   }
 }
-/** Require an array of Stuff documents in the props. */
 Profile.propTypes = {
-  profile: PropTypes.array.isRequired,
   ready: PropTypes.bool.isRequired,
 };
 
 /** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
 export default withTracker(() => {
-  // Get access to Stuff documents.
+  // Get access to Profile documents.
   const subscription = Meteor.subscribe('Profile');
   return {
-    profile: Profiles.find({}).fetch(),
+    profile: Profiles.findOne({}),
     ready: subscription.ready(),
   };
 })(Profile);
