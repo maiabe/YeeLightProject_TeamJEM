685603f6f119ca67bd77b8abc763b1e890013729
Julie Chai <juliechai75@gmail.com>
2019-04-19 21:32:04 -1000

    update

--- app/imports/ui/pages/UserHome.jsx
+++ app/imports/ui/pages/UserHome.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Container, Loader, Button, Card, Label, Grid } from 'semantic-ui-react';
+import { Container, Loader, Card, Label, Grid } from 'semantic-ui-react';
 import { Profiles } from '/imports/api/profile/profile';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
@@ -21,7 +21,6 @@ class UserHome extends React.Component {
     const duration = (this.props.profile == null) ? 5 : this.props.profile.duration;
     const cycle = (this.props.profile == null) ? 28 : this.props.profile.cycle;
     this.state = {
-      willSet: null,
       duration: duration,
       cycle: cycle,
       period: [],
@@ -46,14 +45,8 @@ class UserHome extends React.Component {
                 left: 'prev,next today',
                 center: 'title',
               }}
-              /*customButtons = {{
-                inputPeriod: {
-                  text: 'Period', click: function () {
-                  }
-                }
-              }}*/
-              events={[this.state.period, this.state.pms]}
-              dateClick={this.handleDateClick}
+              events = {this.state.period}
+              dateClick = { this.handleDateClick }
           />
           <Card>
             <Card.Content>
@@ -80,64 +73,48 @@ class UserHome extends React.Component {
           </Card>
 
         </Container>
-
     );
   }
 
   handleDateClick = (clicked) => {
-      this.state.period = [];
-      let first = clicked.date;
-      let last;
+    this.state.period = [];
+    this.state.pms = [];
+    let first, last, pmsStart, pmsEnd;
 
-      for (let i = 0; i < 12; i++) {
-        first = new Date();
-        first.setDate(clicked.date.getDate() + i * this.state.cycle);
-        last = new Date(first.toDateString());
-        last.setDate(first.getDate() + this.state.duration);
-        this.setState({   // add next predicted period to events array
-          period: this.state.period.concat({
-            groupId: first.toDateString(),
-            title: 'period',
-            start: first,
-            end: last,
-            allDay: true,
-            backgroundColor: 'red'
-          })
-        });
+    for (let i = 0; i < 12; i++) {
+      first = new Date();
+      first.setDate(clicked.date.getDate() + i * this.state.cycle);
+      last = new Date(first.toDateString());
+      last.setDate(first.getDate() + this.state.duration);
 
-        const today = new Date();
-        const periodNow = this.state.period.find(period => today.getTime() > period.start.getTime() &&
-            today.getTime() < period.end.getTime());
-        if (periodNow) {
-          console.log("Currently on period - turn bulb orange.");
-        }
-      }
+      pmsStart = new Date(first.toDateString());
+      pmsStart.setDate(pmsStart.getDate() - 5);
+      pmsEnd = new Date(first.toDateString());
 
-    let last = new Date(clicked.date.toDateString());
-    last.setDate(last.getDate() + this.state.duration);
-    let pmsStart = new Date(clicked.date.toDateString());
-    pmsStart.setDate(pmsStart.getDate() - 5);
-    let pmsEnd = new Date(clicked.date.toDateString());
-    pmsEnd.setDate(pmsEnd.getDate());
-    this.setState({
-      period: {
-        title: 'Period',
-        start: clicked.date,
-        end: last,
-        allDay: clicked.allDay,
-        backgroundColor: '#DB2828'
-      },
-      pms: {
-        title: 'PMS',
-        start: pmsStart,
-        end: pmsEnd,
-        allDay: clicked.allDay,
-        backgroundColor: '#FBBD08'
-      }
-    });
+      this.setState({
+        period: this.state.period.concat({
+          title: 'Period',
+          start: first,
+          end: last,
+          allDay: true,
+          backgroundColor: '#DB2828'
+        }),
+        pms: this.state.pms.concat({
+          title: 'PMS',
+          start: pmsStart,
+          end: pmsEnd,
+          allDay: true,
+          backgroundColor: '#FBBD08'
+        })
+      });
 
-    //this.state.period.push({title: 'period', start: clicked.date, end: last });
-    // console.log(this.state.period);
+      /*const today = new Date();
+      const periodNow = this.state.period.find(period => today.getTime() > period.start.getTime() &&
+          today.getTime() < period.end.getTime());
+      if (periodNow) {
+        console.log("Currently on period - turn bulb orange.");
+      }*/
+    }
   }
 }
 
@@ -155,4 +132,3 @@ export default withTracker(() => {
     ready: subscription.ready(),
   };
 })(UserHome);
-
