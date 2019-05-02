413186ca095234326341fff6d5e0d5e3837b4e43
sc30 <maiabe@hawaii.edu>
2019-05-01 22:20:39 -1000

    LightColor functions
    added lightcolor functions
    different light color ct for pms and period and normal state

--- app/imports/api/yeelightbulb/yeelight.js
+++ app/imports/api/yeelightbulb/yeelight.js
@@ -53,6 +53,100 @@ Meteor.methods({
     });
 
   },
+  restartBright() {
+    const discover = new yeelight.Discover({
+      port: 1982,
+      debug: true
+    });
+
+    console.log("0+++++++++++++++++++++++++++++REACHED!!!!!!!!!!!");
+
+    discover.once("deviceAdded", (device) => {
+      bulb = new yeelight.Yeelight({
+        lightIp: device.host,
+        lightPort: device.port
+      });
+
+      bulb.on("connected", () => {
+        let lightPower = bulb.setBright(100);
+        lightPower.then(function (value) {
+          console.log("1++++++++++++++++++++++++++++++++++Light on! ", value);
+        });
+
+        lightPower.catch(function (reason) {
+          console.log("2------------------------------Problem Encountered: ", reason);
+        });
+      });
+
+      let bulbConnect = bulb.connect();
+
+      bulbConnect.then(function (value) {
+        console.log("2+++++++++++++++++++++++++++++++++++Connected to light! ", value);
+      });
+
+      bulbConnect.catch(function (reason) {
+        console.log("2----------------------------------Problem Encountered: ", reason);
+      });
+    });
+
+    let bulbDiscover = discover.start();
+
+    bulbDiscover.then(function (value) {
+      console.log("3-------------------------------------Searching..............", value);
+    });
+
+    bulbDiscover.catch(function (reason) {
+      console.log("4-------------------------------------Problem Encountered", reason);
+    });
+
+  },
+  restartCT() {
+    const discover = new yeelight.Discover({
+      port: 1982,
+      debug: true
+    });
+
+    console.log("0+++++++++++++++++++++++++++++REACHED!!!!!!!!!!!");
+
+    discover.once("deviceAdded", (device) => {
+      bulb = new yeelight.Yeelight({
+        lightIp: device.host,
+        lightPort: device.port
+      });
+
+      bulb.on("connected", () => {
+        let lightPower = bulb.setCtAbx(3000);
+        lightPower.then(function (value) {
+          console.log("1++++++++++++++++++++++++++++++++++Light on! ", value);
+        });
+
+        lightPower.catch(function (reason) {
+          console.log("2------------------------------Problem Encountered: ", reason);
+        });
+      });
+
+      let bulbConnect = bulb.connect();
+
+      bulbConnect.then(function (value) {
+        console.log("2+++++++++++++++++++++++++++++++++++Connected to light! ", value);
+      });
+
+      bulbConnect.catch(function (reason) {
+        console.log("2----------------------------------Problem Encountered: ", reason);
+      });
+    });
+
+    let bulbDiscover = discover.start();
+
+    bulbDiscover.then(function (value) {
+      console.log("3-------------------------------------Searching..............", value);
+    });
+
+    bulbDiscover.catch(function (reason) {
+      console.log("4-------------------------------------Problem Encountered", reason);
+    });
+
+  },
 
   toggleBulb() {
     console.log("Toggle Bulb Button Pressed");
@@ -150,6 +244,148 @@ Meteor.methods({
     bulbDiscover.catch(function (reason) {
       console.log("4-------------------------------------Problem Encountered", reason);
     });
+  },
+
+  pmsNotify() {
+    const discover = new yeelight.Discover({
+      port: 1982,
+      debug: true
+    });
+
+    console.log("0+++++++++++++++++++++++++++++REACHED!!!!!!!!!!!");
+
+    discover.once("deviceAdded", (device) => {
+      bulb = new yeelight.Yeelight({
+        lightIp: device.host,
+        lightPort: device.port
+      });
+
+      bulb.on("connected", () => {
+        let lightPower = bulb.setCtAbx(4500);
+        lightPower.then(function (value) {
+          console.log("1++++++++++++++++++++++++++++++++++Light on! ", value);
+        });
+
+        lightPower.catch(function (reason) {
+          console.log("2------------------------------Problem Encountered: ", reason);
+        });
+      });
+
+      let bulbConnect = bulb.connect();
+
+      bulbConnect.then(function (value) {
+        console.log("2+++++++++++++++++++++++++++++++++++Connected to light! ", value);
+      });
+
+      bulbConnect.catch(function (reason) {
+        console.log("2----------------------------------Problem Encountered: ", reason);
+      });
+    });
+
+    let bulbDiscover = discover.start();
+
+    bulbDiscover.then(function (value) {
+      console.log("3-------------------------------------Searching..............", value);
+    });
+
+    bulbDiscover.catch(function (reason) {
+      console.log("4-------------------------------------Problem Encountered", reason);
+    });
+
+  },
+  periodNotify() {
+    const discover = new yeelight.Discover({
+      port: 1982,
+      debug: true
+    });
+
+    console.log("0+++++++++++++++++++++++++++++REACHED!!!!!!!!!!!");
+
+    discover.once("deviceAdded", (device) => {
+      bulb = new yeelight.Yeelight({
+        lightIp: device.host,
+        lightPort: device.port
+      });
+
+      bulb.on("connected", () => {
+        let lightPower = bulb.setCtAbx(2700);
+        lightPower.then(function (value) {
+          console.log("1++++++++++++++++++++++++++++++++++Light on! ", value);
+        });
+
+        lightPower.catch(function (reason) {
+          console.log("2------------------------------Problem Encountered: ", reason);
+        });
+      });
+
+      let bulbConnect = bulb.connect();
+
+      bulbConnect.then(function (value) {
+        console.log("2+++++++++++++++++++++++++++++++++++Connected to light! ", value);
+      });
+
+      bulbConnect.catch(function (reason) {
+        console.log("2----------------------------------Problem Encountered: ", reason);
+      });
+    });
+
+    let bulbDiscover = discover.start();
+
+    bulbDiscover.then(function (value) {
+      console.log("3-------------------------------------Searching..............", value);
+    });
+
+    bulbDiscover.catch(function (reason) {
+      console.log("4-------------------------------------Problem Encountered", reason);
+    });
+
+  },
+  normalCT() {
+    const discover = new yeelight.Discover({
+      port: 1982,
+      debug: true
+    });
+
+    console.log("0+++++++++++++++++++++++++++++REACHED!!!!!!!!!!!");
+
+    discover.once("deviceAdded", (device) => {
+      bulb = new yeelight.Yeelight({
+        lightIp: device.host,
+        lightPort: device.port
+      });
+
+      bulb.on("connected", () => {
+        let lightPower = bulb.setCtAbx(6500);
+        lightPower.then(function (value) {
+          console.log("1++++++++++++++++++++++++++++++++++Light on! ", value);
+        });
+
+        lightPower.catch(function (reason) {
+          console.log("2------------------------------Problem Encountered: ", reason);
+        });
+      });
+
+      let bulbConnect = bulb.connect();
+
+      bulbConnect.then(function (value) {
+        console.log("2+++++++++++++++++++++++++++++++++++Connected to light! ", value);
+      });
+
+      bulbConnect.catch(function (reason) {
+        console.log("2----------------------------------Problem Encountered: ", reason);
+      });
+    });
+
+    let bulbDiscover = discover.start();
+
+    bulbDiscover.then(function (value) {
+      console.log("3-------------------------------------Searching..............", value);
+    });
+
+    bulbDiscover.catch(function (reason) {
+      console.log("4-------------------------------------Problem Encountered", reason);
+    });
+
   },
 
 });
